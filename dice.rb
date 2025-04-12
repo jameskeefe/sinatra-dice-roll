bad220a0103f348e3606829911c485c1f0f21329
jameskeefe <204756923+jameskeefe@users.noreply.github.com>
2025-04-12 23:55:35 +0000

    checkpoint

--- dice.rb
+++ dice.rb
@@ -10,6 +10,17 @@ use(BetterErrors::Middleware)
 BetterErrors.application_root = __dir__
 BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
+get("/") do
+  "
+  <h1>Dice Roll</h1>
+  <ul>
+    <li><a href=\"/dice/2/6\">Roll two 6-sided dice</a></li>
+    <li><a href=\"/dice/2/10\">Roll two 10-sided dice</a></li>
+    <li><a href=\"/dice/1/20\">Roll one 20-sided die</a></li>
+    <li><a href=\"/dice/5/4\">Roll five 4-sided dice</a></li>
+  </ul>
+  "
+end
 
 get("/howdy") do
   return "Hello, world!"
@@ -17,14 +28,8 @@ end
 
 get("/zebra") do
   "We must add a route for each path we want to support"
-  # it only actually returns the last thing
-  "JK"
 end
 
-get("/") do
-  lucky_num = rand(100)
-  return "Your lucky number is #{lucky_num}."
-end
 
 get("/dice/2/6") do
   first_dice = rand(1..6)
