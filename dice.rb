732f851723b70ab5c9f3308697832c33f0541214
jameskeefe <204756923+jameskeefe@users.noreply.github.com>
2025-04-13 00:17:27 +0000

    Checkpoint - created layout.erb file and finsihed up 2/6

--- dice.rb
+++ dice.rb
@@ -11,15 +11,7 @@ BetterErrors.application_root = __dir__
 BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get("/") do
-  "
-  <h1>Dice Roll</h1>
-  <ul>
-    <li><a href=\"/dice/2/6\">Roll two 6-sided dice</a></li>
-    <li><a href=\"/dice/2/10\">Roll two 10-sided dice</a></li>
-    <li><a href=\"/dice/1/20\">Roll one 20-sided die</a></li>
-    <li><a href=\"/dice/5/4\">Roll five 4-sided dice</a></li>
-  </ul>
-  "
+  erb(:elephant)
 end
 
 get("/howdy") do
@@ -36,10 +28,9 @@ get("/dice/2/6") do
   second_dice = rand(1..6)
   sum = first_dice + second_dice
 
-  outcome = "You rolled a #{first_dice} and a #{second_dice} for a total of #{sum}."
+  @outcome = "You rolled a #{first_dice} and a #{second_dice} for a total of #{sum}."
 
-  "<h1>2d6</h1>
-  <p>#{outcome}</p>"
+  erb(:two_six)
 end
 
 get("/dice/2/10") do
